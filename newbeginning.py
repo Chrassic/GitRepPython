a17ba03f17d54e86212188fd361a45d3e47488e1
Chrassic <62022858+Chrassic@users.noreply.github.com>
2020-03-11 00:04:14 +0100

    Update newbeginning.py

--- newbeginning.py
+++ newbeginning.py
@@ -1,18 +1,3 @@
-""" geburtsjahr = input ("Gib dein Geburtsjahr an: ")
-alter = 2020 - int(geburtsjahr)
-print (type(alter))
-print (alter) """
-
-""" name = raw_input ("Dein Name? ")
-farbe = raw_input ("Deine Lieblingsfarbe? ")
-print ("Hi " + name + ", deine Lieblingsfarbe ist also " + farbe) """
-
-"""name = raw_input ("Wie ist dein Name? ")
-gewicht_pfund = input ("Ok, " + name + ". Gib nun bitte dein Gewicht in Pfund an: ")
-gewicht_kilogramm = int (gewicht_pfund)* 0.45
-print("Du wiegst also " + str(gewicht_kilogramm) + " Kilogramm, " + name + "; du bist ganz schoen fett")
-//Das essenzielle hierbei ist 'str', weil es den string in eine zahl konvertiert"""
-
 print ("Willkommen\n")
 print ("Zum Oeffnen des Tresors benoetigen Sie eine Kombination.\n")
 
@@ -21,7 +6,7 @@ x1 = raw_input ("Zweite Zahl: ")
 x2 = raw_input ("Dritte Zahl: ")
 if x == '2' and x1 == '5' and x2 == '9': #Alle drei müssen richtig sein!
   print ("Richtige Kombination.\n")
-  print ("Jetzt fehlt noch eine Zahl. ")
+  print ("Bitte geben Sie die finale Zahl ein. ")
   if raw_input () == '1':
     print ("Danke. Das Programm wird fortgesetzt.")
   else:
