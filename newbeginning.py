d03d0876830126a57493855bf2a91643e6988a93
Chrassic <62022858+Chrassic@users.noreply.github.com>
2020-03-11 00:02:10 +0100

    Update newbeginning.py

--- newbeginning.py
+++ newbeginning.py
@@ -13,8 +13,6 @@ gewicht_kilogramm = int (gewicht_pfund)* 0.45
 print("Du wiegst also " + str(gewicht_kilogramm) + " Kilogramm, " + name + "; du bist ganz schoen fett")
 //Das essenzielle hierbei ist 'str', weil es den string in eine zahl konvertiert"""
 
-#STOP BEI 29.29 (Python-Video, Youtube)
-
 print ("Willkommen\n")
 print ("Zum Oeffnen des Tresors benoetigen Sie eine Kombination.\n")
 
@@ -25,7 +23,7 @@ if x == '2' and x1 == '5' and x2 == '9': #Alle drei müssen richtig sein!
   print ("Richtige Kombination.\n")
   print ("Jetzt fehlt noch eine Zahl. ")
   if raw_input () == '1':
-    print ("Danke.")
+    print ("Danke. Das Programm wird fortgesetzt.")
   else:
     raw_input ("Sie haben die falsche Zahl eingegeben. Das Programm endet hier.")
 else:
