9d3f27ee5eb2190e78fe55a83b7865de81d1984b
Chrassic <62022858+Chrassic@users.noreply.github.com>
2020-03-13 22:31:39 +0100

    Update create_class.py

--- create_class.py
+++ create_class.py
@@ -1,9 +1,10 @@
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age, height):
       self.name = name
       self.age = age
+      self.height = height
     def myfunc(self):
-        print ("Mein Name ist " + self.name + " und ich bin " + str(self.age) + " Jahre alt.") #str ist elementar, um int in str zu konvertieren
-
-p1 = Person("Chris", 28)
+        print ("Mein Name ist " + self.name + " und ich bin " + str(self.age) + " Jahre alt" + " sowie " + str(self.height) + "m gross") #str ist elementar, um int in str zu konvertieren
+        print (2+200*4+5-107+100*5)
+p1 = Person("Chris", 28, 1.70)
 p1.myfunc()
