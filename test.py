98f6622537d814b746f0fae665be4e11177b46ec
Chrassic <62022858+Chrassic@users.noreply.github.com>
2020-03-22 15:38:14 +0100

    ...

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
-import fibo
-fibo.fib (10)
+#import fibo
+#fibo.fib (10)
+
+weight = int(input("Weight: "))
+unit = input("(L)bs or (K)g: ")
+if unit.upper() == "L":
+    weight_in_kg = weight*0.45
+    print (f"You are {weight_in_kg} kg.")
+elif unit.upper() == "K":
+    weight_in_lbs = weight/0.45
+    print (f"You are {weight_in_lbs} lbs.")
+else:
+    print ("Wrong input.")
+#ytvid: 1:20:49
