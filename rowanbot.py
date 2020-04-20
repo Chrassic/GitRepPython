5d82e172cae9e89d8717b6d807414bb359d00c86
Chrassic <62022858+Chrassic@users.noreply.github.com>
2020-04-20 21:47:40 +0200

    Create rowanbot.py

---
+++ rowanbot.py
@@ -0,0 +1,14 @@
+#Rowan-Bot
+import random
+
+rowan1 = ["Alan,", "Adam,", "Ellie,", "Byron,"]
+rowan2 = ["from an official Playtech standpoint:", "I am telling you:", "you are asking for a payrise?", "hang on..."]
+rowan3 = ["That's a no", "This is outrageous", "This is a Playtech-special which means Playtech gets a special treatment", "I am waiting for my thirty thousand Dollar bonus muahahaha"]
+
+rowans = [rowan1, rowan2, rowan3]
+rowan_sentence = [] #Liste rowan_sentence
+
+for rowan in rowans:
+    r = random.randint(0, len(rowan)-1)
+    rowan_sentence.append(rowan[r]) #Füge in die Liste rowan_sentence eine der rowan-Listen mit einem r ein, also einer Zufallszahl
+print (" ".join(rowan_sentence)+"!") #Konvertiere die Liste rowan_sentence in einen Satz mit .join
